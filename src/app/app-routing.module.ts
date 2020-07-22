9b30b1b6762d0be9bb184f37e3fab9415c34138d
tarek <tarek.bettaieb@esprit.tn>
2020-07-22 20:45:32 +0100

    login template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'landing-page',
+    component:LandingPageComponent
+  }, 
+  {
+    path:'login',
+    component:LoginComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
-import { SliderSectionComponent } from './Components/slider-section/slider-section.component';
-import { AboutSectionComponent } from './Components/about-section/about-section.component';
-import { ProgramSectionComponent } from './Components/program-section/program-section.component';
-import { EventSectionComponent } from './Components/event-section/event-section.component';
-import { ComingSoonSectionComponent } from './Components/coming-soon-section/coming-soon-section.component';
-import { TestimonialSectionComponent } from './Components/testimonial-section/testimonial-section.component';
-import { CallSectionComponent } from './Components/call-section/call-section.component';
-import { TeamSectionComponent } from './Components/team-section/team-section.component';
-import { ContactSectionComponent } from './Components/contact-section/contact-section.component';
-import { NewsSectionComponent } from './Components/news-section/news-section.component';
-import { ClientSectionComponent } from './Components/client-section/client-section.component';
+import { LoginComponent } from './Components/login/login.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SliderSectionComponent,
-    AboutSectionComponent,
-    ProgramSectionComponent,
-    EventSectionComponent,
-    ComingSoonSectionComponent,
-    TestimonialSectionComponent,
-    CallSectionComponent,
-    TeamSectionComponent,
-    ContactSectionComponent,
-    NewsSectionComponent,
-    ClientSectionComponent
+    LoginComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
