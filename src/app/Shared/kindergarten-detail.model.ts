c45c9f4756223428c3496e205a98e5dc6194dc26
tarek <tarek.bettaieb@esprit.tn>
2020-07-27 19:13:31 +0100

    Program model&service

---
+++ src/app/Shared/kindergarten-detail.model.ts
@@ -0,0 +1,13 @@
+export class KindergartenDetail {
+    KindergartenId!: number;
+    Name!: string;
+    Description!: string;
+    Address!: string;
+    Cost!: number; 
+    Phone!: number;
+    DateCreation!: number;
+    Logo!: string;
+    NbrEmp!: number;
+    
+}
+
